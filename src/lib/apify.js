cf520ccd5f7f0832a4d58a330eac85437cf4a870
umarprogrammer19 <umarprogrammer19666@gmail.com>
2026-03-06 21:12:46 +0500

    Facebook Scrapper Added

--- src/lib/apify.js
+++ src/lib/apify.js
@@ -35,3 +35,31 @@ export async function scrapeTwitter(limit = 10) {
         author_name: item.author?.userName || "Unknown",
     }));
 }
+
+export async function scrapeFacebook(limit = 10, url = null) {
+    const defaultUrls = [
+        "https://www.facebook.com/groups/3280541332233338",
+        "https://www.facebook.com/groups/domainbusiness",
+        "https://www.facebook.com/groups/bestwebhostingdomainflip",
+        "https://www.facebook.com/groups/domainnamegroup",
+        "https://www.facebook.com/groups/saasfounders"
+    ];;
+    const targetUrl = url || defaultUrls[Math.floor(Math.random() * defaultUrls.length)];
+
+    const input = {
+        startUrls: [{ url: targetUrl }],
+        resultsLimit: parseInt(limit),
+        viewOption: "CHRONOLOGICAL",
+        useProxy: true
+    };
+
+    const run = await client.actor("apify/facebook-groups-scraper").call(input);
+    const { items } = await client.dataset(run.defaultDatasetId).listItems();
+
+    return items.map(item => ({
+        platform: 'facebook',
+        content: item.text || "",
+        url: item.url,
+        author_name: item.user?.name || "Unknown",
+    }));
+}
